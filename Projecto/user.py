5898dd5d73458be454bf21805d92a64beaffeac9
andreffs18 <andreffs18@gmail.com>
2015-09-21 09:31:22 +0100

    first version of user.py

--- Projecto/user.py
+++ Projecto/user.py
@@ -1,47 +1,57 @@
-import utils.global_utils; utils.global_utils.read_env() ## read .env vars
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# import utils.global_utils; utils.global_utils.read_env() ## read .env vars
+# from utils.global_utils import ArgOrigin, error_nc, error, logger, handle_args
+# from utils.socket_utils import udp_client_request, tcp_client_request, tcp_client_request_send_and_receive, tcp_client_request_open, tcp_client_request_close
 
 import sys, os
-from utils.global_utils import ArgOrigin, error_nc, error, logger, handle_args
-from utils.socket_utils import udp_client_request, tcp_client_request, tcp_client_request_send_and_receive, tcp_client_request_open, tcp_client_request_close
 
+# Default port of the ECP server
+ECP_PORT = 58054
+# Default name of the ECP server
+ECP_NAME = 'lima'
 
 if __name__ == "__main__":
-    logger("Starting client...")
-        
-        ## handling arguments
-        cshost, csport = handle_args(sys.argv, ArgOrigin.CLIENT)
-        sshost, ssport = None, None
-        
-        ## for the while loop
-        status = True
-        while(status):
-            ## input user arguments
-            arg = raw_input("Select command: ").split(" ")
-                
-                if arg[0] == "list":
-                    sshost, ssport = _list(cshost, csport)
-                elif arg[0] == "retrieve":
-                    try:
-                        if ssport is None or sshost is None:
-                            error_nc(__name__, "No Storage Server Found. Call \"list\" first.")
-                                elif arg[1] == '' or arg[1].isspace():
-                                    raise IndexError
-                                else:
-                                    _retrieve(arg[1], sshost, ssport)
-                    except IndexError:
-                        error_nc(__name__, "No filename.\nusage: retrieve <filename>")
-                            
-                            elif arg[0] == "upload":	
-                                try:
-                                if arg[1] == '' or arg[1].isspace():
-                                    raise IndexError
-                                _upload(arg[1], cshost, csport)
-                                    except IndexError:
-                                        error_nc(__name__, "No filename.\nusage: upload <filename>")
-                                            
-                                            elif arg[0] == "exit":
-                                                status = not status
-                                                    else:
-                                                        error_nc(__name__, "Option Not Valid.")
-                                                    
-    logger("Exiting client...")
+    print("Starting client...")
+
+    ## handling arguments
+    args = sys.argv
+    if len(args) > 5:
+        print("Error: too many arguments. Please use form ./user [-n ECPname] [-p ECPport]")
+        sys.exit()
+
+    if "-n" in args:
+        ECPname = args[2]
+    else:
+        ECPname = ECP_NAME
+
+    if "-p" in args:
+        ECPport = args[4]
+    else:
+        ECPport = ECP_PORT
+
+    print("Using ECPname = {} and ECPport = {}".format(ECPname, ECPport))
+
+
+    ## for the while loop
+    while(True):
+
+        # 4 commands
+        # list - chama o ECP com UDP as protocol. pede a lista de topicos
+        # request - Topic NUm (Tnn) por TCP chama o TES respectivo
+        # submit <answers_sequece> - TCP Tes resposta
+        # exit - exit
+
+        input_data = raw_input("")
+
+        if 'list' in input_data:
+            print("listing stuf > {}".format(input_data))
+        elif 'request' in input_data:
+            print("requesting stuf > {}".format(input_data))
+        elif 'submit' in input_data:
+            print("stuf tem dois éfes > {}".format(input_data))
+        elif 'exit' in input_data:
+            break
+
+    print("Exiting client...")
